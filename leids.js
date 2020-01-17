3feeaebc6ae926ddb1d3cdae26f0777b61ecf1e0
donbrae <jamie@donbrae.co.uk>
2020-01-17 21:13:45 +0000

    Added volume object to leids.js

--- leids.js
+++ leids.js
@@ -1,18 +1,28 @@
 jQuery(document).ready(function($) {
 
+  var volume = {
+    sco: 0.7
+  };
+
   $('.play').click(function(e) {
 
     // Unique variables for click event
     var $button = $(this),
+      lang = $button.data('file').split('__')[1],
       audio;
 
     $button.prop('disabled', true);
     audio = document.getElementById($button.data('file'));
 
+    if (volume[lang]) {
+      audio.volume = volume[lang];
+    }
+
     $(audio).bind('ended', function() {
       $(this).unbind('ended');
       $button.prop('disabled', false);
     });
+
     audio.play();
   });
 });
